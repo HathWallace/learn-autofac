4ae80fb4323c7c90e52d809cea1e2ed338d959bc
HathWallace <hathzhou@qq.com>
2021-10-11 22:50:28 +0800

    Winform中尝试Autofac

---
+++ ConsoleApplication/AutoFac/AutoFacStartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Autofac;
+
+namespace ConsoleApplication.AutoFac
+{
+    public static class AutoFacStartUp
+    {
+        private static readonly ContainerBuilder _builder;
+        private static readonly IContainer _container;
+
+        static AutoFacStartUp()
+        {
+            _builder = new ContainerBuilder();
+
+            var assemblies = AppDomain
+                .CurrentDomain
+                .GetAssemblies();
+            _builder
+                .RegisterAssemblyTypes(assemblies)
+                .AsImplementedInterfaces()
+                .AsSelf()
+                .PropertiesAutowired(new AutowiredPropertySelector(), true);
+            _container = _builder.Build();
+        }
+
+        public static T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+    }
+}
---
+++ WindowsFormsApplication/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ConsoleApplication.AutoFac;
+using InterfaceLib;
+
+namespace WindowsFormsApplication
+{
+    public partial class MainForm : Form
+    {
+        [Autowired]
+        private ILogic _logic { get; set; }
+
+        //[Autowired]
+        //private ILogic1 _logic1 { get; set; }
+
+        //[Autowired]
+        //private ILogic2 _logic2 { get; set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _logic.Test();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var _logic1 = AutoFacStartUp
+                .Resolve<ILogic1>();
+            _logic1.Test();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //_logic2.Test();
+        }
+    }
+}
